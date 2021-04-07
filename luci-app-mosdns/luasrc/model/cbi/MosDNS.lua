51d893e9d8ab9c40825d7bf89f0b13a5f0407732
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-04-08 00:24:18 +0800

    Update MosDNS.lua

--- luci-app-mosdns/luasrc/model/cbi/MosDNS.lua
+++ luci-app-mosdns/luasrc/model/cbi/MosDNS.lua
@@ -1,5 +1,8 @@
 require("luci.sys")
 require("luci.util")
+require("luci.http")
+require("luci.dispatcher")
+require("nixio.fs")
 local fs=require"nixio.fs"
 local port=require"luci.model.uci".cursor()
 local port=port:get("MosDNS","MosDNS","port")
@@ -15,6 +18,21 @@ s.addremove=false
 enabled = s:option(Flag, "enabled", translate("启用MosDNS"))
 enabled.default = 0
 enabled.rmempty = false
+
+-- manual-config
+addr = s:option(Value, "manual-config", translate("手动配置"))
+
+addr.template = "cbi/tvalue"
+addr.rows = 20
+
+function addr.cfgvalue(self, section)
+	return nixio.fs.readfile("/etc/mosdns/config.yaml")
+end
+
+function addr.write(self, section, value)
+	value = value:gsub("\r\n?", "\n")
+	nixio.fs.writefile("/etc/mosdns/config.yaml", value)
+end
 
 local apply = luci.http.formvalue("cbi.apply")
  if apply then
