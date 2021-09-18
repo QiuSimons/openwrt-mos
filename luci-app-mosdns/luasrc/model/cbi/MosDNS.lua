d46114e0162c1c4c86ea98c3280f877b392c772b
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2021-09-18 12:46:21 +0800

    Update MosDNS.lua

--- luci-app-mosdns/luasrc/model/cbi/MosDNS.lua
+++ luci-app-mosdns/luasrc/model/cbi/MosDNS.lua
@@ -1,45 +1,31 @@
-require("luci.sys")
-require("luci.util")
-require("luci.http")
-require("luci.dispatcher")
-require("nixio.fs")
-local fs=require"nixio.fs"
-local port=require"luci.model.uci".cursor()
-local port=port:get("MosDNS","MosDNS","port")
+
 
 mp = Map("MosDNS", translate("MosDNS"))
+mp.title = translate("MosDNS")
 mp.description = translate("MosDNS is a 'programmable' DNS forwarder.")
-mp:section(SimpleSection).template  = "MosDNS/MosDNS_status"
+mp:section(SimpleSection).template = "MosDNS/MosDNS_status"
 
 s = mp:section(TypedSection, "MosDNS")
-s.anonymous=true
-s.addremove=false
+s.addremove = false
+s.anonymous = true
 
-enabled = s:option(Flag, "enabled", translate("Enable"))
-enabled.default = 0
-enabled.rmempty = false
+s:tab("basic", translate("Basic Setting"))
+enable = s:taboption("basic",Flag, "enabled", translate("Enable"))
+enable.rmempty = false
 
--- manual-config
-addr = s:option(Value, "manual-config", translate("Manual Configuration"),
-translate("------------------------------------------------------------------------------------------------------------"))
 
-addr.template = "cbi/tvalue"
-addr.rows = 25
+s:tab("manual-config", translate("Manual Configuration"))
+config = s:taboption("manual-config", Value, "manual-config", translate("Manual Configuration"), translate("This file is /etc/mosdns/config.yaml."), "")
+config.template = "cbi/tvalue"
+config.rows = 25
 
-function addr.cfgvalue(self, section)
+function config.cfgvalue(self, section)
 	return nixio.fs.readfile("/etc/mosdns/config.yaml")
 end
 
-function addr.write(self, section, value)
+function config.write(self, section, value)
 	value = value:gsub("\r\n?", "\n")
 	nixio.fs.writefile("/etc/mosdns/config.yaml", value)
 end
-
-local apply = luci.http.formvalue("cbi.apply")
- if apply then
-     io.popen("/etc/init.d/MosDNS reload")
-end
-
-io.popen("/etc/init.d/MosDNS reload")
 
 return mp
