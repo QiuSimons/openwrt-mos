3103fb8ccfda62649f26e7bbf4be88d7af7bc54d
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-09-19 22:04:07 +0800

    cron

---
+++ luci-app-mosdns/files/mosdns/mosupdater.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+set -o pipefail
+rm -rf  /tmp/mosdns
+mkdir /tmp/mosdns
+wget https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -nv -O /tmp/mosdns/geoip.dat
+wget https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -nv -O /tmp/mosdns/geosite.dat
+find /tmp/chinadns-ng/ -size -20k -exec rm {} \;
+chmod -R  755  /tmp/mosdns
+cp -rf /tmp/mosdns/** /etc/mosdns
+exit 0
