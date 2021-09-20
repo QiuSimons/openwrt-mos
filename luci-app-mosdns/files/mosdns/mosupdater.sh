dc83f5cbf5ce153cec0faae36f8726f88e9ee9b6
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-09-20 09:54:37 +0800

    Update mosupdater.sh

--- luci-app-mosdns/files/mosdns/mosupdater.sh
+++ luci-app-mosdns/files/mosdns/mosupdater.sh
@@ -4,7 +4,7 @@ rm -rf  /tmp/mosdns
 mkdir /tmp/mosdns
 wget https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -nv -O /tmp/mosdns/geoip.dat
 wget https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -nv -O /tmp/mosdns/geosite.dat
-find /tmp/chinadns-ng/ -size -20k -exec rm {} \;
+find /tmp/mosdns/ -size -20k -exec rm {} \;
 chmod -R  755  /tmp/mosdns
 cp -rf /tmp/mosdns/* /etc/mosdns
 exit 0
