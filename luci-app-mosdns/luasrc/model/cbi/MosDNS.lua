1e46b5b9b6b162e58d43ff5466534e8ba46fab00
QiuSimons <QiuSimons@users.noreply.github.com>
2021-10-06 21:01:11 +0800

    Revert "add Check And Update GEOIP Database"
    This reverts commit 47edab7686adc0f5d231180bb49e6b64480d6388.

--- luci-app-mosdns/luasrc/model/cbi/MosDNS.lua
+++ luci-app-mosdns/luasrc/model/cbi/MosDNS.lua
@@ -11,30 +11,6 @@ s:tab("basic", translate("Basic Setting"))
 enable = s:taboption("basic",Flag, "enabled", translate("Enable"))
 enable.rmempty = false
 
-o = s:taboption("geo_update", ListValue, "geo_update_week_time", translate("Update Time (Every Week)"))
-o:value("*", translate("Every Day"))
-o:value("1", translate("Every Monday"))
-o:value("2", translate("Every Tuesday"))
-o:value("3", translate("Every Wednesday"))
-o:value("4", translate("Every Thursday"))
-o:value("5", translate("Every Friday"))
-o:value("6", translate("Every Saturday"))
-o:value("0", translate("Every Sunday"))
-o.default=1
-
-o = s:taboption("geo_update", ListValue, "geo_update_day_time", translate("Update time (every day)"))
-for t = 0,23 do
-o:value(t, t..":00")
-end
-o.default=0
-
-o = s:taboption("geo_update", Button, translate("GEOIP Update")) 
-o.title = translate("Update GEOIP Database")
-o.inputtitle = translate("Check And Update")
-o.inputstyle = "reload"
-o.write = function()
-  luci.sys.exec("/etc/mosdns/mosupdater.sh >/dev/null 2>&1 &")
-end
 
 s:tab("manual-config", translate("Manual Configuration"))
 config = s:taboption("manual-config", Value, "manual-config", translate("Manual Configuration"), translate("This file is /etc/mosdns/config.yaml."), "")
