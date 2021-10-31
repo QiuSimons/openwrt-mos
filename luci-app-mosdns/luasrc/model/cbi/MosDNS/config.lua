15cf69689b3ddd12599f10baa5cdead3acbeef86
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-10-31 15:56:01 +0000

    luci-app-mosdns: tidy up luci

---
+++ luci-app-mosdns/luasrc/model/cbi/MosDNS/config.lua
@@ -0,0 +1,21 @@
+m = Map("MosDNS")
+
+s = m:section(TypedSection, "MosDNS")
+s.addremove = false
+s.anonymous = true
+
+config = s:option(Value, "manual-config", translate("Manual Configuration"))
+config.description = translate("View the YAML Configuration file used by this MosDNS. You can edit it as you own need; Beware the listening port 5335 was hardcoded into the init script, do not change that.")
+config.template = "cbi/tvalue"
+config.rows = 25
+
+function config.cfgvalue(self, section)
+  return nixio.fs.readfile("/etc/mosdns/config.yaml")
+end
+
+function config.write(self, section, value)
+  value = value:gsub("\r\n?", "\n")
+  nixio.fs.writefile("/etc/mosdns/config.yaml", value)
+end
+
+return m
