58f0db04abfc387361d7daee41f1d456bd2e5190
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-10-31 15:56:01 +0000

    luci-app-mosdns: tidy up luci

--- luci-app-mosdns/luasrc/controller/MosDNS.lua
+++ luci-app-mosdns/luasrc/controller/MosDNS.lua
@@ -1,9 +1,14 @@
 module("luci.controller.MosDNS", package.seeall)
+
 function index()
-	if not nixio.fs.access("/etc/config/MosDNS")then
+	if not nixio.fs.access("/etc/config/MosDNS") then
 		return
 	end
-	entry({"admin", "services", "MosDNS"}, cbi("MosDNS"), _("MosDNS"), 30).dependent = true
+
+	entry({"admin", "services", "MosDNS"}, alias("admin", "services", "MosDNS", "basic"), _("MosDNS"), 30).dependent = true
+	entry({"admin", "services", "MosDNS", "basic"}, cbi("MosDNS/basic"), _("Basic Setting"), 1).leaf = true
+	entry({"admin", "services", "MosDNS", "update"}, cbi("MosDNS/update"), _("Geodata Update"), 2).leaf = true
+	entry({"admin", "services", "MosDNS", "config"}, cbi("MosDNS/config"), _("Manual Configuration"), 3).leaf = true
 	entry({"admin", "services", "MosDNS", "status"}, call("act_status")).leaf = true
 end
 
