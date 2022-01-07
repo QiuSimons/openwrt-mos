3b3e838b0d2a94ac6ef559f2ea3838422f94c939
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2022-01-07 23:57:32 +0800

    luci: bug fix and update

--- luci-app-mosdns/luasrc/model/cbi/mosdns/basic.lua
+++ luci-app-mosdns/luasrc/model/cbi/mosdns/basic.lua
@@ -54,4 +54,19 @@ unset_config.write = function()
   luci.sys.exec("/etc/mosdns/unset.sh >/dev/null 2>&1 &")
 end
 
+config = s:option(TextValue, "manual-config")
+config.description = translate("<font color=\"ff0000\"><strong>View the Custom YAML Configuration file used by this MosDNS. You can edit it as you own need.")
+config.template = "cbi/tvalue"
+config.rows = 25
+config:depends( "configfile", "./cus_config.yaml")
+
+function config.cfgvalue(self, section)
+  return nixio.fs.readfile("/etc/mosdns/cus_config.yaml")
+end
+
+function config.write(self, section, value)
+  value = value:gsub("\r\n?", "\n")
+  nixio.fs.writefile("/etc/mosdns/cus_config.yaml", value)
+end
+
 return m
