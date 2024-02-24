4be63ede09b532455457843e409ba5476b51f2da
daiaji <hentaiwushuang@gmail.com>
2024-02-25 00:19:19 +0800

    重构
    使用GPT重构了大量的代码，让代码更规范更现代化。
    
    添加功能：
    1. 现在可以在LUCI设置反代地址了，workers.dev在我这不太好用，基本上报废了.
    2. 现在你可以在使用“自定义配置”时也能切换“启用 DNS 广告过滤”选项了。

--- luci-app-mosdns/luasrc/model/cbi/mosdns/basic.lua
+++ luci-app-mosdns/luasrc/model/cbi/mosdns/basic.lua
@@ -1,86 +1,94 @@
-m = Map("mosdns")
-m.title = translate("MosDNS")
+-- Function to add remote DNS values
+local function add_remote_dns_values(remote_dns)
+  remote_dns:value("tls://8.8.8.8", "8.8.8.8 (Google DNS)")
+  remote_dns:value("tls://8.8.4.4", "8.8.4.4 (Google DNS)")
+  remote_dns:value("tls://1.1.1.1", "1.1.1.1 (CloudFlare DNS)")
+  remote_dns:value("tls://1.0.0.1", "1.0.0.1 (CloudFlare DNS)")
+  remote_dns:value("tls://101.101.101.101", "101.101.101.101 (Quad101 DNS)")
+  remote_dns:value("tls://9.9.9.11", "9.9.9.11 (Quad9 DNS)")
+  remote_dns:value("tls://149.112.112.11", "149.112.112.11 (Quad9 DNS)")
+  remote_dns:value("tls://208.67.222.222", "208.67.222.222 (Open DNS)")
+  remote_dns:value("tls://208.67.220.220", "208.67.220.220 (Open DNS)")
+  remote_dns:value("tls://94.140.14.140", "94.140.14.140 (AdGuard)")
+  remote_dns:value("tls://94.140.14.141", "94.140.14.141 (AdGuard)")
+end
+
+-- Create the map
+local m = Map("mosdns", translate("MosDNS"))
 m.description = translate("MosDNS is a 'programmable' DNS forwarder.")
 
-m:section(SimpleSection).template = "mosdns/mosdns_status"
+-- Add the status section
+local s = m:section(SimpleSection)
+s.template = "mosdns/mosdns_status"
 
+-- Add the main section
 s = m:section(TypedSection, "mosdns")
 s.addremove = false
 s.anonymous = true
 
-enable = s:option(Flag, "enabled", translate("Enable"))
+-- Enable option
+local enable = s:option(Flag, "enabled", translate("Enable"))
 enable.rmempty = false
 
-configfile = s:option(ListValue, "configfile", translate("MosDNS Config File"))
+-- Config file option
+local configfile = s:option(ListValue, "configfile", translate("MosDNS Config File"))
 configfile:value("./def_config.yaml", translate("Def Config"))
 configfile:value("./cus_config.yaml", translate("Cus Config"))
 configfile.default = "./def_config.yaml"
 
-listenport = s:option(Value, "listen_port", translate("Listen port"))
+-- Listen port option
+local listenport = s:option(Value, "listen_port", translate("Listen port"))
 listenport.datatype = "and(port,min(1))"
 listenport.default = 5335
-listenport:depends( "configfile", "./def_config.yaml")
+listenport:depends("configfile", "./def_config.yaml")
 
-loglv = s:option(ListValue, "loglv", translate("Log Level"))
+-- Log level option
+local loglv = s:option(ListValue, "loglv", translate("Log Level"))
 loglv:value("debug", translate("Debug"))
 loglv:value("info", translate("Info"))
 loglv:value("warn", translate("Warning"))
 loglv:value("error", translate("Error"))
 loglv.default = "error"
-loglv:depends( "configfile", "./def_config.yaml")
+loglv:depends("configfile", "./def_config.yaml")
 
-logfile = s:option(Value, "logfile", translate("MosDNS Log File"))
+-- Log file option
+local logfile = s:option(Value, "logfile", translate("MosDNS Log File"))
 logfile.placeholder = "/tmp/mosdns.txt"
 logfile.default = "/tmp/mosdns.txt"
-logfile:depends( "configfile", "./def_config.yaml")
+logfile:depends("configfile", "./def_config.yaml")
 
-remote_dns = s:option(Value, "remote_dns1", translate("Remote DNS"))
+-- Remote DNS options
+local remote_dns = s:option(Value, "remote_dns1", translate("Remote DNS"))
 remote_dns.default = "tls://1.0.0.1"
-remote_dns:value("tls://8.8.8.8", "8.8.8.8 (Google DNS)")
-remote_dns:value("tls://8.8.4.4", "8.8.4.4 (Google DNS)")
-remote_dns:value("tls://1.1.1.1", "1.1.1.1 (CloudFlare DNS)")
-remote_dns:value("tls://1.0.0.1", "1.0.0.1 (CloudFlare DNS)")
-remote_dns:value("tls://101.101.101.101", "101.101.101.101 (Quad101 DNS)")
-remote_dns:value("tls://9.9.9.11", "9.9.9.11 (Quad9 DNS)")
-remote_dns:value("tls://149.112.112.11", "149.112.112.11 (Quad9 DNS)")
-remote_dns:value("tls://208.67.222.222", "208.67.222.222 (Open DNS)")
-remote_dns:value("tls://208.67.220.220", "208.67.220.220 (Open DNS)")
-remote_dns:value("tls://94.140.14.140", "94.140.14.140 (AdGuard)")
-remote_dns:value("tls://94.140.14.141", "94.140.14.141 (AdGuard)")
-remote_dns:depends( "configfile", "./def_config.yaml")
-remote_dns = s:option(Value, "remote_dns2", "ㅤ")
-remote_dns.default = "tls://208.67.220.220"
-remote_dns:value("tls://8.8.8.8", "8.8.8.8 (Google DNS)")
-remote_dns:value("tls://8.8.4.4", "8.8.4.4 (Google DNS)")
-remote_dns:value("tls://1.1.1.1", "1.1.1.1 (CloudFlare DNS)")
-remote_dns:value("tls://1.0.0.1", "1.0.0.1 (CloudFlare DNS)")
-remote_dns:value("tls://101.101.101.101", "101.101.101.101 (Quad101 DNS)")
-remote_dns:value("tls://9.9.9.11", "9.9.9.11 (Quad9 DNS)")
-remote_dns:value("tls://149.112.112.11", "149.112.112.11 (Quad9 DNS)")
-remote_dns:value("tls://208.67.222.222", "208.67.222.222 (Open DNS)")
-remote_dns:value("tls://208.67.220.220", "208.67.220.220 (Open DNS)")
-remote_dns:value("tls://94.140.14.140", "94.140.14.140 (AdGuard)")
-remote_dns:value("tls://94.140.14.141", "94.140.14.141 (AdGuard)")
-remote_dns:depends( "configfile", "./def_config.yaml")
+remote_dns:depends("configfile", "./def_config.yaml")
+add_remote_dns_values(remote_dns)
 
-redirect = s:option(Flag, "redirect", translate("Enable DNS Redirect"))
-redirect:depends( "configfile", "./def_config.yaml")
+local remote_dns2 = s:option(Value, "remote_dns2", "ㅤ")
+remote_dns2.default = "tls://208.67.220.220"
+remote_dns2:depends("configfile", "./def_config.yaml")
+add_remote_dns_values(remote_dns2)
+
+-- DNS Redirect option
+local redirect = s:option(Flag, "redirect", translate("Enable DNS Redirect"))
+redirect:depends("configfile", "./def_config.yaml")
 redirect.default = true
 
-adblock = s:option(Flag, "adblock", translate("Enable DNS ADblock"))
-adblock:depends( "configfile", "./def_config.yaml")
+-- DNS ADblock option
+local adblock = s:option(Flag, "adblock", translate("Enable DNS ADblock"))
 adblock.default = false
 
-set_config = s:option(Button, "set_config", translate("DNS Helper"))
+-- DNS Helper button
+local set_config = s:option(Button, "set_config", translate("DNS Helper"))
 set_config.inputtitle = translate("Apply")
 set_config.inputstyle = "reload"
 set_config.description = translate("This will make the necessary adjustments to other plug-in settings.")
 set_config.write = function()
   luci.sys.exec("/etc/mosdns/set.sh &> /dev/null &")
 end
-set_config:depends( "configfile", "./def_config.yaml")
+set_config:depends("configfile", "./def_config.yaml")
 
-unset_config = s:option(Button, "unset_config", translate("Revert Settings"))
+-- Revert Settings button
+local unset_config = s:option(Button, "unset_config", translate("Revert Settings"))
 unset_config.inputtitle = translate("Apply")
 unset_config.inputstyle = "reload"
 unset_config.description = translate("This will revert the adjustments.")
--- luci-app-mosdns/luasrc/model/cbi/mosdns/rule_list.lua
+++ luci-app-mosdns/luasrc/model/cbi/mosdns/rule_list.lua
@@ -1,10 +1,34 @@
 local datatypes = require "luci.cbi.datatypes"
 
-local white_list_file = "/etc/mosdns/rule/whitelist.txt"
-local block_list_file = "/etc/mosdns/rule/blocklist.txt"
-local hosts_list_file = "/etc/mosdns/rule/hosts.txt"
-local redirect_list_file = "/etc/mosdns/rule/redirect.txt"
-local cus_config_file = "/etc/mosdns/cus_config.yaml"
+local rule_files = {
+  white_list = "/etc/mosdns/rule/whitelist.txt",
+  block_list = "/etc/mosdns/rule/blocklist.txt",
+  hosts_list = "/etc/mosdns/rule/hosts.txt",
+  redirect_list = "/etc/mosdns/rule/redirect.txt",
+  cus_config = "/etc/mosdns/cus_config.yaml"
+}
+
+local function read_file(file_path)
+  local file = io.open(file_path, "r")
+  if file then
+    local content = file:read("*a")
+    file:close()
+    return content
+  end
+  return ""
+end
+
+local function write_file(file_path, content)
+  local file = io.open(file_path, "w")
+  if file then
+    file:write(content)
+    file:close()
+  end
+end
+
+local function remove_file(file_path)
+  write_file(file_path, "")
+end
 
 m = Map("mosdns")
 
@@ -20,56 +44,56 @@ s:tab("cus_config", translate("Cus Config"))
 o = s:taboption("white_list", TextValue, "whitelist", "", "<font color='red'>" .. translate("These domain names allow DNS resolution with the highest priority. Please input the domain names of websites, every line can input only one website domain. For example: hm.baidu.com.") .. "</font>" .. "<font color='#00bd3e'>" .. translate("<br>The list of rules only apply to 'Default Config' profiles.") .. "</font>")
 o.rows = 15
 o.wrap = "off"
-o.cfgvalue = function(self, section) return nixio.fs.readfile(white_list_file) or "" end
-o.write = function(self, section, value) nixio.fs.writefile(white_list_file , value:gsub("\r\n", "\n")) end
-o.remove = function(self, section, value) nixio.fs.writefile(white_list_file , "") end
+o.cfgvalue = function(self, section) return read_file(rule_files.white_list) end
+o.write = function(self, section, value) write_file(rule_files.white_list, value:gsub("\r\n", "\n")) end
+o.remove = function(self, section, value) remove_file(rule_files.white_list) end
 o.validate = function(self, value)
-    return value
+  return value
 end
 
 o = s:taboption("block_list", TextValue, "blocklist", "", "<font color='red'>" .. translate("These domains are blocked from DNS resolution. Please input the domain names of websites, every line can input only one website domain. For example: baidu.com.") .. "</font>" .. "<font color='#00bd3e'>" .. translate("<br>The list of rules only apply to 'Default Config' profiles.") .. "</font>")
 o.rows = 15
 o.wrap = "off"
-o.cfgvalue = function(self, section) return nixio.fs.readfile(block_list_file) or "" end
-o.write = function(self, section, value) nixio.fs.writefile(block_list_file, value:gsub("\r\n", "\n")) end
-o.remove = function(self, section, value) nixio.fs.writefile(block_list_file, "") end
+o.cfgvalue = function(self, section) return read_file(rule_files.block_list) end
+o.write = function(self, section, value) write_file(rule_files.block_list, value:gsub("\r\n", "\n")) end
+o.remove = function(self, section, value) remove_file(rule_files.block_list) end
 o.validate = function(self, value)
-    return value
+  return value
 end
 
 o = s:taboption("hosts_list", TextValue, "hosts", "", "<font color='red'>" .. translate("Hosts For example: baidu.com 10.0.0.1") .. "</font>" .. "<font color='#00bd3e'>" .. translate("<br>The list of rules only apply to 'Default Config' profiles.") .. "</font>")
 o.rows = 15
 o.wrap = "off"
-o.cfgvalue = function(self, section) return nixio.fs.readfile(hosts_list_file) or "" end
-o.write = function(self, section, value) nixio.fs.writefile(hosts_list_file, value:gsub("\r\n", "\n")) end
-o.remove = function(self, section, value) nixio.fs.writefile(hosts_list_file, "") end
+o.cfgvalue = function(self, section) return read_file(rule_files.hosts_list) end
+o.write = function(self, section, value) write_file(rule_files.hosts_list, value:gsub("\r\n", "\n")) end
+o.remove = function(self, section, value) remove_file(rule_files.hosts_list) end
 o.validate = function(self, value)
-    return value
+  return value
 end
 
 o = s:taboption("redirect_list", TextValue, "redirect", "", "<font color='red'>" .. translate("The domain name to redirect the request to. Requests domain A, but returns records for domain B. example: a.com b.com") .. "</font>" .. "<font color='#00bd3e'>" .. translate("<br>The list of rules only apply to 'Default Config' profiles.") .. "</font>")
 o.rows = 15
 o.wrap = "off"
-o.cfgvalue = function(self, section) return nixio.fs.readfile(redirect_list_file) or "" end
-o.write = function(self, section, value) nixio.fs.writefile(redirect_list_file, value:gsub("\r\n", "\n")) end
-o.remove = function(self, section, value) nixio.fs.writefile(redirect_list_file, "") end
+o.cfgvalue = function(self, section) return read_file(rule_files.redirect_list) end
+o.write = function(self, section, value) write_file(rule_files.redirect_list, value:gsub("\r\n", "\n")) end
+o.remove = function(self, section, value) remove_file(rule_files.redirect_list) end
 o.validate = function(self, value)
-    return value
+  return value
 end
 
 o = s:taboption("cus_config", TextValue, "Cus Config", "", "<font color='red'>" .. translate("View the Custom YAML Configuration file used by this MosDNS. You can edit it as you own need.") .. "</font>" .. "<font color='#00bd3e'>" .. translate("<br>The list of rules only apply to 'Custom Config' profiles.") .. "</font>")
 o.rows = 30
 o.wrap = "off"
-o.cfgvalue = function(self, section) return nixio.fs.readfile(cus_config_file) or "" end
-o.write = function(self, section, value) nixio.fs.writefile(cus_config_file, value:gsub("\r\n", "\n")) end
-o.remove = function(self, section, value) nixio.fs.writefile(cus_config_file, "") end
+o.cfgvalue = function(self, section) return read_file(rule_files.cus_config) end
+o.write = function(self, section, value) write_file(rule_files.cus_config, value:gsub("\r\n", "\n")) end
+o.remove = function(self, section, value) remove_file(rule_files.cus_config) end
 o.validate = function(self, value)
-    return value
+  return value
 end
 
 local apply = luci.http.formvalue("cbi.apply")
 if apply then
-    luci.sys.exec("/etc/init.d/mosdns reload")
+  luci.sys.exec("/etc/init.d/mosdns reload")
 end
 
 return m
