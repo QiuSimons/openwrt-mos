fc1b66c1f45e529f9890a1dc28bdfc43d3cc63cf
daiaji <hentaiwushuang@gmail.com>
2024-03-19 20:09:20 +0800

    修复比较整数带来的故障 https://github.com/QiuSimons/openwrt-mos/pull/163#issuecomment-2006855106 就像这里所说。

--- luci-app-mosdns/root/etc/mosdns/mosupdater.sh
+++ luci-app-mosdns/root/etc/mosdns/mosupdater.sh
@@ -14,9 +14,9 @@ getdat geosite_cn.txt
 getdat geosite_no_cn.txt
 getdat geoip_cn.txt
 
-[ "$adblock" -eq 1 ] && getdat serverlist.txt
+[ "$adblock" == "1" ] && getdat serverlist.txt
 
-if [ "$syncconfig" -eq 1 ]; then
+if [ "$syncconfig" == "1" ]; then
   getdat def_config_v5.yaml
   mv "$TMPDIR"/def_config_v5.yaml "$TMPDIR"/def_config_orig.yaml
   cp -rf "$TMPDIR"/def_config_orig.yaml /etc/mosdns
